639d6d615fab0d1d2b62764dce26671bb134426a
Wendy <wli025@ucr.edu>
2019-07-20 16:47:46 -0700

    added basic form

--- tamborine/src/App.js
+++ tamborine/src/App.js
@@ -22,21 +22,46 @@
 // }
 
 // export default App;
-import React from 'react'
-import Navigation from './components/navigation'
+import React from "react";
+import Navigation from "./components/navigation";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-      <div>
-        <h1>tamborine.</h1>
+        <div id="header">
+          <h1>tamborine.</h1>
+          <h4 id="subheader">find out what works best for you</h4>
+        </div>
+        <div class="container">
+          {/* NAVBAR */}
+          <div class="tambo-nav">
+            <Navigation />
+          </div>
+          {/* FORM */}
+          <div class="tambo-form">
+            <h4>add item.</h4>
+            <form action="/action_page.php" method="get">
+              <input
+                type="text"
+                name="productname"
+                placeholder="product name"
+              />
+              <br />
+              <input type="text" name="company" placeholder="company" />
+              <br />
+              <input
+                type="text"
+                name="ingredients"
+                placeholder="ingredients, separated by commas"
+              />
+              <br />
+              <input type="submit" />
+            </form>
+          </div>
+        </div>
       </div>
-      <div>
-        <Navigation></Navigation>\
-      </div>
-      </div>
-    )
+    );
   }
 }
-export default App
+export default App;
--- tamborine/src/components/navigation.js
+++ tamborine/src/components/navigation.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import styled from "styled-components"
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const List = styled.ul`
-    z-index: 10;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
+  z-index: 10;
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 
-    & > li {
-        width: 57px;
-        margin: 20px;
-        font-size: 16px;
-        font-weight: semi-bold;
-        overflow: visible;
+  & > li {
+    width: 57px;
+    margin: 20px;
+    font-size: 16px;
+    font-weight: semi-bold;
+    overflow: visible;
 
     & > a {
       color: #333234;
@@ -24,19 +24,19 @@ const List = styled.ul`
       }
     }
   }
-`
+`;
 
 class Navigation extends React.Component {
-    render () {
-        return (
-            <List>
-                <Link to="/navigation" >my items</Link>
-                <li>my items</li>
-                <li>add product</li>
-                <li>product lookup</li>
-            </List>
-        )
-    }
+  render() {
+    return (
+      <List>
+        {/* <Link to="/navigation" >my items</Link> */}
+        <li>my items</li>
+        <li>add product</li>
+        <li>product lookup</li>
+      </List>
+    );
+  }
 }
 
-export default Navigation
+export default Navigation;
