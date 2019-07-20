d34a9247c1800664272d5a74be8946cd94715eea
natashaorie <norie001@ucr.edu>
2019-07-20 12:57:39 -0700

    set up routing

--- tamborine/src/App.js
+++ tamborine/src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import './App.css';
+// import React from 'react';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+import React from 'react'
+import Navigation from './components/navigation'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+      <div>
+        <h1>tamborine.</h1>
+      </div>
+      <div>
+        <Navigation></Navigation>\
+      </div>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ tamborine/src/components/navigation.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import styled from "styled-components"
+import { Link } from 'react-router-dom'
+
+const List = styled.ul`
+    z-index: 10;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+
+    & > li {
+        width: 57px;
+        margin: 20px;
+        font-size: 16px;
+        font-weight: semi-bold;
+        overflow: visible;
+
+    & > a {
+      color: #333234;
+      text-decoration: none;
+
+      &:hover {
+        color: #aa6da3;
+      }
+    }
+  }
+`
+
+class Navigation extends React.Component {
+    render () {
+        return (
+            <List>
+                <Link to="/navigation" />
+                <li>my items</li>
+                <li>add product</li>
+                <li>product lookup</li>
+            </List>
+        )
+    }
+}
+
+export default Navigation
