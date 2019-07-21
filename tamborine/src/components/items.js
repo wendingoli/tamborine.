af2f783c4734272f8b7f4cad9f77c717178e98ba
Wendy <wli025@ucr.edu>
2019-07-20 18:45:31 -0700

    added images

--- tamborine/src/components/items.js
+++ tamborine/src/components/items.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import i3 from "../img/3.jpg";
 
 const List = styled.ul`
   z-index: 10;
@@ -8,7 +9,11 @@ const List = styled.ul`
   margin: 0;
   padding: 0;
 
-  & > product {
+  img {
+    height: 100%;
+    width: 100%;
+  }
+  & > .product {
     width: 200px;
     height: 200px;
     background: red;
@@ -36,14 +41,18 @@ class Items extends React.Component {
     return (
       <List>
         <div class="container myitems">
-          <div class="product" />
-          <div class="product" />
-          <div class="product" />
-          <div class="product" />
-          <div class="product" />
-          <div class="product" />
-          <div class="product" />
-          <div class="product" />
+          <div class="product">
+            {" "}
+            <img src={i3} />{" "}
+          </div>
+          <div class="product">
+            {" "}
+            <img src="https://www.sephora.com/productimages/sku/s2189389-main-zoom.jpg" />{" "}
+          </div>
+          <div class="product">
+            {" "}
+            <img src="https://www.sephora.com/productimages/sku/s2042794-main-Lhero.jpg" />{" "}
+          </div>
         </div>
       </List>
     );
